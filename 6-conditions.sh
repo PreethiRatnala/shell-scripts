6398af791577914e3453f7c1bac706de0e1ff00a
PreethiRatnala <preethi.ratnala64@gmail.com>
2024-05-07 18:22:10 +0530

    conditions

--- 6-conditions.sh
+++ 6-conditions.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-number=echo "enter your number$1"
-
+echo "enter your number"
+number=$1
 read number
 
 if [ $number -gt 4 ];then
-echo "number greater than 4"
+echo "$number greater than 4"
 else
-echo "number less than 4"
+echo "$number less than 4"
 fi;
 
